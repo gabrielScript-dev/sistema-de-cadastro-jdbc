65716534d97b4940811256079f7cd5a89c40e433
Gabriel Pereira <gabriel.script@gmail.com>
2022-12-12 17:47:55 -0300

    Update Main.java
    Teste de Inserção na base de dados.

--- src/br/com/sistema/Main.java
+++ src/br/com/sistema/Main.java
@@ -1,9 +1,20 @@
 package br.com.sistema;
 
+import br.com.sistema.dao.ContatoDao;
+import br.com.sistema.modelo.Contato;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ContatoDao dao = new ContatoDao();
+		
+		Contato contato = new Contato();
+		contato.setNome("Gabriel");
+		contato.setEmail("gabriel.script@gmail.com");
+		contato.setEndereco("R. Paes Cabral");
+		
+		dao.adicionar(contato);
 
 	}
 

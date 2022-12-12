bdff0a76920d6be590ba513c02e6e4a2c3d42094
Gabriel Pereira <gabriel.script@gmail.com>
2022-12-12 17:48:15 -0300

    Update ConnectionFactory.java
    Implementação da classe de conexão com o banco de dados.

--- src/br/com/sistema/jdbc/ConnectionFactory.java
+++ src/br/com/sistema/jdbc/ConnectionFactory.java
@@ -1,5 +1,17 @@
 package br.com.sistema.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
 
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection("jdbc:mysql://localhost/testebd", "root","qwe12");
+			
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
